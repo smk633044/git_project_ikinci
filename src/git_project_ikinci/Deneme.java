0d676ac71440e4d19e36cedb23d00d56e890bf78
smk633044 <69387929+smk633044@users.noreply.github.com>
2020-08-21 20:39:03 +0200

    Update Deneme.java
    hamzadan selam

--- src/git_project_ikinci/Deneme.java
+++ src/git_project_ikinci/Deneme.java
@@ -4,7 +4,6 @@ public class Deneme {
 
 	public static void main(String[] args) {
 
-		
 		System.out.println("Ikinci git projem");
 		System.out.println("ikinci satir kodum");
 		System.out.println("remote degisiklik");
