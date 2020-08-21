22dad5ee4e123f760eee876473db41ffbd8a545e
Baki <smk633044@gmail.com>
2020-08-21 20:36:36 +0200

    ikinci projemin cakisma commiti

--- src/git_project_ikinci/Deneme.java
+++ src/git_project_ikinci/Deneme.java
@@ -4,12 +4,11 @@ public class Deneme {
 
 	public static void main(String[] args) {
 
-		
 		System.out.println("Ikinci git projem");
 		System.out.println("ikinci satir kodum");
 		System.out.println("remote degisiklik");
 		System.out.println("Baki");
-		
+		System.out.println("Cakisma - Baki");
 	}
 
 }
